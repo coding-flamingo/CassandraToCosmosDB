ae0582fc9d14c47c859d143da7f2f905d383c19e
igal <igalfsg@gmail.com>
2021-11-08 19:23:07 -0500

    added parser

---
+++ CassandraToAzureFiles/Program.cs
@@ -0,0 +1,84 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CassandraToAzureFiles.models;
+using System.Text;
+using System.Text.Json;
+
+Console.WriteLine("Enter full path to your Cassandra File");
+string fileContents = String.Empty;
+string filepath = Console.ReadLine().Trim();
+try
+{
+    using (var sr = new StreamReader(filepath))
+    {
+        fileContents = sr.ReadToEnd();
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine("The file could not be read:");
+    Console.WriteLine(e.Message);
+    return;
+}
+if(string.IsNullOrEmpty(fileContents))
+{
+    Console.WriteLine("Error file is empty");
+    return;
+}
+string[] tables = fileContents.Split("CREATE TABLE");
+List< CassandraTableModel > azTables = new ();
+foreach(string table in tables)
+{
+    if(!string.IsNullOrWhiteSpace(table))
+    {
+        string[] lines = table.Split("\n");
+        CassandraTableModel newTable = new CassandraTableModel();
+        newTable.Name = lines[0].Split("(")[0].Trim();
+        for (int i = 1; i < lines.Length; i++)
+        {
+            if (!string.IsNullOrWhiteSpace(lines[i]))
+            {
+                if (lines[i].Contains("PRIMARY KEY"))
+                {
+                    string line = lines[i].Trim()
+                        .Replace("PRIMARY KEY", "").TrimEnd(')')
+                        .TrimEnd(')').Replace("(","");
+                    foreach (string s in line.Split(","))
+                    {
+                        newTable.jsonContents.PartitionKeys.Add(new()
+                        {
+                            Name = s.Trim(),
+                        });
+                    }
+                }
+                else
+                {
+                    string line = lines[i].Trim().TrimEnd(',');
+                    newTable.jsonContents.Columns.Add(new()
+                    {
+                        Name = line.Split(" ")[0],
+                        Type = line.Split(" ")[1]
+                    });
+                }
+            }
+        }
+        azTables.Add(newTable);
+    }
+}
+string powershell = string.Empty;
+foreach (CassandraTableModel table in azTables)
+{
+    string jsondata = JsonSerializer.Serialize(table.jsonContents);
+    using (FileStream fs = File.Create("output\\" + table.Name + ".json"))
+    {
+        byte[] info = new UTF8Encoding(true).GetBytes(jsondata);
+        fs.Write(info, 0, info.Length);
+    }
+    powershell += "az cosmosdb cassandra table update" +
+        " --account-name codingflamingotestserver --keyspace-name testkeyspace" +
+        $" --name {table.Name} --resource-group cosmosTest --schema '@{table.Name}.json' \n";
+}
+using (FileStream fs = File.Create("output\\createTables.ps1"))
+{
+    byte[] info = new UTF8Encoding(true).GetBytes(powershell);
+    fs.Write(info, 0, info.Length);
+}
---
+++ CassandraToAzureFiles/models/CassandraTableModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CassandraToAzureFiles.models
+{
+    public class CassandraTableModel
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = string.Empty;
+        public JsonObj jsonContents { get; set; } = new JsonObj();
+    }
+    public class JsonObj
+    {
+        [JsonPropertyName("columns")]
+        public List<Columns> Columns { get; set; } = new();
+        [JsonPropertyName("partitionKeys")]
+        public List<Keys> PartitionKeys { get; set; } = new();
+    }
+    public class Columns
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = string.Empty;
+        [JsonPropertyName("type")]
+        public string Type { get; set; } = string.Empty;
+    }
+    public class Keys
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = string.Empty;
+    }
+}
